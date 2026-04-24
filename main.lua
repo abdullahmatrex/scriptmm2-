c8743c560087b9d6d8eefc2ba01e5bf53c5f4257
abdullahmatrex <abdullahzxcvbnmpp@gmail.com>
2026-04-24 13:29:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- [[ CYBER DRAGON V22 - THE ULTIMATE ARABIC EDITION ]]
 -- المطور: عبد الله (Dev=abdullah)
 -- تيك توك: 7_v4n3x | تليجرام: abood_7ro
--- 35 ملاحظة: تم دمج جميع الأزرار والوظائف (أكثر من  ميزة)
+--ملاحظة: تم دمج جميع الأزرار والوظائف (أكثر من  ميزة 35)
 
 local Players = game:GetService("Players")
 local Lighting = game:GetService("Lighting")
